b07fbef96a5a975b57b3166964ae0f42e0fee65f
xyㅋ <127842244+xyz987164@users.noreply.github.com>
2023-12-12 14:21:08 +0900

    Update app.py

--- app.py
+++ app.py
@@ -21,7 +21,7 @@ def load_df():
 def check_rows(column,options):
     return res.loc[res[column].isin(options)]
 
-st.title("Demo DataFrame Quary App")
+st.title("Demo DataFrame Query App")
 
 df,survival_options,p_class_options,sex_options,embark_options,min_fare,max_fare,min_age,max_age =load_df()
 res=df
